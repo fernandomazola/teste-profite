08acd77b42f18fb857eb6e14cf11df6331cbe1ec
fernandomazola <fernando_mazola@hotmail.com>
2019-07-27 12:18:17 -0300

    Teste Parcialmente Finalizado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,71 @@
-//FUNCIONALIDADE MENU
+
 $(document).ready(function(){
     
-    $('.tog').click(function(){
-        var altura = $('.list-mobile').height();
-        if(altura > 0){
-            $('.list-mobile').removeClass('active');
-            $('.list-mobile').toggleClass('disable');
-        }else if(altura == 0){
-            $('.list-mobile').removeClass('disable');
-            $('.list-mobile').toggleClass('active');
-        }        
-    });
+    //FUNCIONALIDADE SLIDESHOW
+    //FUNCIONALIDADE SLIDESHOW
+    var imgItens = $('.slider li').length; //Verifica quantidade de imagens que tem na página
+    var imgPos = 1;
+
+    //Adicionando a paginação
+    for(i = 1; i <= imgItens; i++){
+        $('.pagination').append('<li><span class="fas fa-circle"></span></li>');
+    }
+    
+    $('.slider li').hide(); //Esconder todos os sliders assim que carrega a página
+    $('.slider li:first-child').show(); //Mostrar apenas o primeiro slider
+    $('.pagination li:first').css({'color': '#FF9F1C'}); // Estilo para o primeiro item da paginação
+
+
+    // Executar funções
+    //Ao clicar na paginação ele envia para o slide respectivo
+    $('.pagination li').click(pagination);
+    //Ao clicar nas flechas envia para o prox ou slide anterior
+    $('.next-right span').click(nextSlider);
+    $('.next-left span').click(prevSlider);
+
+
+    // setInterval(function(){
+    //     nextSlider();
+    // },4000);
+
+    //FUNÇÕES
+    function pagination(){
+        var paginationPos = $(this).index() + 1;
+        $('.slider li').hide();
+        $('.slider li:nth-child('+paginationPos+')').fadeIn();
+        $('.pagination li').css({'color': '#BDBDBD'});
+        $(this).css({'color': '#FF9F1C'});
+
+        imgPos = paginationPos;
+    }
+
+    function nextSlider(){
+        if(imgPos >= imgItens){
+            imgPos = 1;
+        }else{
+            imgPos ++;
+        }
+        $('.slider li').hide();
+        $('.slider li:nth-child('+imgPos+')').fadeIn();
+        $('.pagination li').css({'color': '#BDBDBD'});
+        $('.pagination li:nth-child('+imgPos+')').css({'color': '#FF9F1C'});
+    }
+    
+    function prevSlider(){
+        if(imgPos <= 1){
+            imgPos = imgItens;
+        }else{
+            imgPos --;
+        }
+        $('.slider li').hide();
+        $('.slider li:nth-child('+imgPos+')').fadeIn();
+        $('.pagination li').css({'color': '#BDBDBD'});
+        $('.pagination li:nth-child('+imgPos+')').css({'color': '#FF9F1C'});
+    }
+    //FUNCIONALIDADE SLIDESHOW
+    //FUNCIONALIDADE SLIDESHOW
 });
+
+
+
+
